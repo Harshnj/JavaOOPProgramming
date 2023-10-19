a441443c1c052d1b5e096659b2c16c837f74dc4f
Harshnj <95063086+Harshnj@users.noreply.github.com>
2023-10-19 23:22:29 +0530

    Create Simple_Package.java
    A Simple code example for creating a package

---
+++ Encapsulation/package/Simple_Package.java
@@ -0,0 +1,8 @@
+//A java package is a group of similar types of classes, interfaces and sub-packages.
+package com.company;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
