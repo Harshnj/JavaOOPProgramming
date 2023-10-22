6e4fbbdc143dcbdb34fca7b18511c6305f9db3b1
Harshnj <95063086+Harshnj@users.noreply.github.com>
2023-10-23 00:41:14 +0530

    Update abstract_class.java

--- Abstraction/abstract_class.java
+++ Abstraction/abstract_class.java
@@ -1,1 +1,21 @@
+package Abstract.Class;
 
+abstract class Car{
+    abstract void drive(); //Methid is declared but not defined
+
+}
+
+class Wagon extends Car {
+    void drive(){
+        System.out.println("Driving Wagon"); // Method is defined
+    }
+    void music(){
+        System.out.println("Playing");
+    }
+}
+public class Main {
+    public static void main(String[] args) {
+        Car ob = new Wagon();//Method Ovewrring
+        ob.drive();
+         }
+}
