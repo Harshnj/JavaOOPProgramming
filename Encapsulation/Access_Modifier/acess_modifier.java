3bcc911cd300d871d3ab1cf5d784b380d63202bb
Harshnj <95063086+Harshnj@users.noreply.github.com>
2023-10-22 10:52:56 +0530

    Create acess_modifier.java

---
+++ Encapsulation/Access_Modifier/acess_modifier.java
@@ -0,0 +1,12 @@
+//Refer package and import statement for better understanding of where the files are
+package Access_Modifier.subcl;
+
+public class Access_Modifier {
+    public int a=10;
+    protected float b=1.23f;
+
+    long c = 23568;
+
+    private double d= 2.2655;
+
+}
