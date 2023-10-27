720c3df6941206ab7feb7916d99d7c143a8ab446
Harshnj <95063086+Harshnj@users.noreply.github.com>
2023-10-28 00:02:31 +0530

    Create sytax_Error.java

---
+++ Exceptional_Handling/Errors/Complie_Error/sytax_Error.java
@@ -0,0 +1,9 @@
+package Error_Exception;
+
+public class Syntax_Error {
+    int i=0//Missing semicolon
+    in i=0; //Incorrect data type or any keyword name
+    void print(){
+
+    //Missing ending Braces for print method
+}
